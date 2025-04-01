7e68119a5f63875b67f3debe54a031d824256a45
TalionElessar2007 <cagnin.lima.2007@gmail.com>
2025-04-01 20:21:59 -0300

    V1

---
+++ index.js
@@ -0,0 +1,35 @@
+
+let navBar = document.querySelector('#header')
+
+document.addEventListener("scroll", ()=>{
+  let scrollTop = window.scrollY;
+
+  if(scrollTop > 0){
+    navBar.classList.add('rolar');
+  } else{
+    navBar.classList.remove('rolar');
+  }
+
+})
+
+    /*Menu mobile*/
+    let btnMenuMob = document.querySelector('#btn-menu-mob')
+    let line1 = document.querySelector('.line-menumob-1')
+    let line2 = document.querySelector('.line-menumob-2')
+    let menuMobile =document.querySelector('#menu-mobile')
+
+    btnMenuMob.addEventListener("click", ()=>{
+      line1.classList.toggle('ativo1')
+      line2.classList.toggle('ativo2')
+      menuMobile.classList.toggle('abrir')
+
+      body.classList.toggle('no-overflow')
+})
+
+
+let body = document.querySelector('body')
+
+
+function toggleMenu() {
+  document.getElementById("menu-list").classList.toggle("active");
+}
